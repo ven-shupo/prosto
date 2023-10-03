1857d7ee536eab81cf0c6565dd946034e5ae8cb2
opushnev <opushnev.ia@phystech.edu>
2023-10-03 22:02:26 +0300

    Updates

---
+++ precache-manifest.f1439d01d430614be593451546071590.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f06877716fa3b0e3c714e10fd0f2ff9",
+    "url": "/prosto/index.html"
+  },
+  {
+    "revision": "c9f2d0eb1b0244b88292",
+    "url": "/prosto/static/css/main.f77096b2.chunk.css"
+  },
+  {
+    "revision": "cbfb5b2bcf68f12f5dd7",
+    "url": "/prosto/static/js/2.398302f7.chunk.js"
+  },
+  {
+    "revision": "c9f2d0eb1b0244b88292",
+    "url": "/prosto/static/js/main.939d94bf.chunk.js"
+  },
+  {
+    "revision": "b939b04448c707371894",
+    "url": "/prosto/static/js/runtime~main.0bc40996.js"
+  },
+  {
+    "revision": "fce3966ec519d1e11aac4cf3a266f762",
+    "url": "/prosto/static/media/richard.fce3966e.jpg"
+  }
+]);
