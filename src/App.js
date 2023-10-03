8dcd85ce905b4e6c02ccefcd48edfaa0d4307df0
opushnev <opushnev.ia@phystech.edu>
2023-10-03 22:55:05 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css'
 import React from 'react';
 import Advanced from './Advanced'
+import { Helmet } from 'react-helmet';
+
+// const tg = window.Telegram.WebApp;
+
 
 function App () {
   return (
     <div className='app'>
+    <Helmet>
+      <script src="https://telegram.org/js/telegram-web-app.js" />
+    </Helmet>
+      
+    data={window.Telegram}
     <Advanced />
     </div>
   )
